6004c3d64c8c7d2a604c589e24582c8b4aa8e8ef
dev-jarial <pankajjarial.dev@gmail.com>
2025-03-05 18:26:19 +0000

    urls updated

--- company/urls.py
+++ company/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'companies'  # Namespace for URL reversing
+app_name = "company"  # Namespace for URL reversing
 
 urlpatterns = [
     # Main entry point - company details form
-    path('', views.company_form_view, name='company_form'),
-    
+    path("", views.company_form_view, name="company_form"),
     # Optional additional URLs you might want to add
     # path('list/', views.company_list_view, name='company_list'),
     # path('detail/<int:pk>/', views.company_detail_view, name='company_detail'),
