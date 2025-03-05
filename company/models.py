ce762356d4fe75b65f3ccf5c732fb9e0987b1af3
dev-jarial <pankajjarial.dev@gmail.com>
2025-03-05 19:32:24 +0000

    models updated

--- company/models.py
+++ company/models.py
@@ -6,13 +6,13 @@ class Company(models.Model):
     email = models.EmailField(blank=True, null=True)
     mobile_number = models.CharField(max_length=20, blank=True, null=True)
     general_contact_number = models.CharField(max_length=20, blank=True, null=True)
-    hq_address = models.TextField(blank=True, null=True)
+    hq_address = models.JSONField(default=list)
     locations = models.JSONField(default=list)  # Store multiple office locations
 
-    key_capabilities = models.TextField(blank=True, null=True)
+    key_capabilities = models.JSONField(default=list)
     products = models.JSONField(default=list)  # Store list of products
     industry_types = models.JSONField(default=list)  # Store multiple industries
-    partner_category = models.CharField(max_length=255, blank=True, null=True)
+    partner_category = models.JSONField(default=list)
 
     number_of_years = models.PositiveIntegerField(
         blank=True, null=True
@@ -21,12 +21,12 @@ class Company(models.Model):
     number_of_employees = models.PositiveIntegerField(blank=True, null=True)
 
     top_customer_names = models.JSONField(default=list)  # Store list of top customers
-    case_studies_available = models.BooleanField(default=False)
+    case_studies_available = models.JSONField(default=list)
 
     # Store brochure URL instead of boolean
     product_brochure_link = models.URLField(blank=True, null=True)
 
-    client_testimonials = models.TextField(blank=True, null=True)
+    client_testimonials = models.JSONField(default=list)
     oems_working_with = models.JSONField(default=list)  # Store list of OEM partners
     brief_company_profile = models.TextField(blank=True, null=True)
 
