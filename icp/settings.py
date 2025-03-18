1fba76cc219de1b17244dce31ac1818f87748e29
dev-jarial <pankajjarial.dev@gmail.com>
2025-03-18 21:24:07 +0000

    ckeditor added to apps

--- icp/settings.py
+++ icp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "company",
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = "icp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
