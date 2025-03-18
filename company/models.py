7106258ab60ae0fffffe913b20bb2e98fe7bd2ec
dev-jarial <pankajjarial.dev@gmail.com>
2025-03-18 21:23:51 +0000

    field changed to richtext

--- company/models.py
+++ company/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
 
@@ -6,33 +7,33 @@ class Company(models.Model):
     email = models.EmailField(blank=True, null=True)
     mobile_number = models.CharField(max_length=20, blank=True, null=True)
     general_contact_number = models.CharField(max_length=20, blank=True, null=True)
-    hq_address = models.TextField()
-    locations = models.TextField()  # Store multiple office locations
+    hq_address = RichTextField()
+    locations = RichTextField()  # Store multiple office locations
 
-    key_capabilities = models.TextField()
-    products = models.TextField()  # Store list of products
-    industry_types = models.TextField()  # Store multiple industries
-    partner_category = models.TextField()
+    key_capabilities = RichTextField()
+    products = RichTextField()  # Store list of products
+    industry_types = RichTextField()  # Store multiple industries
+    partner_category = RichTextField()
 
     number_of_years = models.CharField(blank=True, null=True)  # Years in business
     number_of_customers = models.CharField(blank=True, null=True)
     number_of_employees = models.CharField(blank=True, null=True)
 
-    top_customer_names = models.TextField()  # Store list of top customers
-    case_studies = models.TextField()
+    top_customer_names = RichTextField()  # Store list of top customers
+    case_studies = RichTextField()
 
     # Store brochure URL instead of boolean
     product_brochure_link = models.URLField(blank=True, null=True)
 
-    client_testimonials = models.TextField()
-    oems_working_with = models.TextField()  # Store list of OEM partners
-    brief_company_profile = models.TextField(blank=True, null=True)
+    client_testimonials = RichTextField()
+    oems_working_with = RichTextField()  # Store list of OEM partners
+    brief_company_profile = RichTextField(blank=True, null=True)
 
-    top_management_details = models.TextField()  # Store management details
+    top_management_details = RichTextField()  # Store management details
     annual_revenue = models.CharField(blank=True, null=True)  # Revenue in USD
     average_deal_size = models.CharField(blank=True, null=True)
 
-    operating_countries = models.TextField()  # List of countries
+    operating_countries = RichTextField()  # List of countries
 
     # top_3_competitors_websites = models.JSONField(
     #     default=list
