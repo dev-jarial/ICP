9a380ac9b7047d546463c88766cfb43e15c6ab99
dev-jarial <pankajjarial.dev@gmail.com>
2025-03-18 22:10:06 +0000

    task queue logic updated

--- company/tasks.py
+++ company/tasks.py
@@ -4,7 +4,8 @@ import json
 from celery import shared_task
 from django.db import connections, transaction
 
-from utils.web_crawler import main
+from utils.list_str import listToStr
+from utils.script import Crawler
 
 from .models import Company
 
@@ -17,7 +18,7 @@ def process_uploaded_file(valid_urls):
             connections.close_all()
 
             # Run the web scraper
-            scraped_data = asyncio.run(main(url=website_link))
+            scraped_data = asyncio.run(Crawler(url=website_link).start())
 
             # Ensure scraped_data is a dictionary
             if isinstance(scraped_data, str):
@@ -28,60 +29,43 @@ def process_uploaded_file(valid_urls):
 
             if not isinstance(scraped_data, dict):
                 continue
-
+            format_data = {
+                "name": scraped_data.get("name"),
+                "email": scraped_data.get("email"),
+                "mobile_number": scraped_data.get("mobile_number"),
+                "general_contact_number": scraped_data.get("general_contact_number"),
+                "hq_address": listToStr(scraped_data.get("hq_address")),
+                "locations": listToStr(scraped_data.get("locations_offices")),
+                "key_capabilities": listToStr(scraped_data.get("key_capabilities")),
+                "products": listToStr(scraped_data.get("products")),
+                "industry_types": listToStr(scraped_data.get("industry_types")),
+                "partner_category": listToStr(scraped_data.get("partner_category")),
+                "number_of_years": scraped_data.get("number_of_years"),
+                "number_of_customers": scraped_data.get("number_of_customers"),
+                "number_of_employees": scraped_data.get("number_of_employees"),
+                "top_customer_names": listToStr(scraped_data.get("top_customer_names")),
+                "case_studies": listToStr(scraped_data.get("case_studies")),
+                "product_brochure_link": scraped_data.get("product_brochure"),
+                "client_testimonials": listToStr(
+                    scraped_data.get("client_testimonials")
+                ),
+                "oems_working_with": listToStr(scraped_data.get("oems_working_with")),
+                "brief_company_profile": scraped_data.get("brief_company_profile"),
+                "top_management_details": listToStr(
+                    scraped_data.get("top_management_details")
+                ),
+                "annual_revenue": scraped_data.get("annual_revenue"),
+                "average_deal_size": scraped_data.get("average_deal_size"),
+                "operating_countries": listToStr(
+                    scraped_data.get("operating_countries")
+                ),
+                "funding_status": scraped_data.get("funding_status"),
+                "google_rating": scraped_data.get("google_rating"),
+            }
             # Ensure atomic database transaction
             with transaction.atomic():
                 Company.objects.update_or_create(
-                    website_link=website_link,
-                    defaults={
-                        "name": scraped_data.get("name", ""),
-                        "email": scraped_data.get("email_id", None),
-                        "mobile_number": scraped_data.get("mobile_number", None),
-                        "general_contact_number": scraped_data.get(
-                            "general_contact_number", None
-                        ),
-                        "hq_address": scraped_data.get("hq_address", []),
-                        "locations": scraped_data.get("locations_offices", []),
-                        "key_capabilities": scraped_data.get("key_capabilities", []),
-                        "products": scraped_data.get("products", []),
-                        "industry_types": scraped_data.get("industry_types", []),
-                        "partner_category": scraped_data.get("partner_category", []),
-                        "number_of_years": scraped_data.get("number_of_years", None),
-                        "number_of_customers": scraped_data.get(
-                            "number_of_customers", None
-                        ),
-                        "number_of_employees": scraped_data.get(
-                            "number_of_employees", None
-                        ),
-                        "top_customer_names": scraped_data.get(
-                            "top_customer_names", []
-                        ),
-                        "case_studies_available": scraped_data.get(
-                            "case_studies_available", []
-                        ),
-                        "product_brochure_link": scraped_data.get(
-                            "product_brochure", None
-                        ),
-                        "client_testimonials": scraped_data.get(
-                            "client_testimonials", []
-                        ),
-                        "oems_working_with": scraped_data.get("oems_working_with", []),
-                        "brief_company_profile": scraped_data.get(
-                            "brief_company_profile", None
-                        ),
-                        "top_management_details": scraped_data.get(
-                            "top_management_details", []
-                        ),
-                        "annual_revenue": scraped_data.get("annual_revenue", None),
-                        "average_deal_size": scraped_data.get(
-                            "average_deal_size", None
-                        ),
-                        "operating_countries": scraped_data.get(
-                            "operating_countries", []
-                        ),
-                        "funding_status": scraped_data.get("funding_status", None),
-                        "google_rating": scraped_data.get("google_rating", None),
-                    },
+                    website_link=website_link, defaults=format_data
                 )
 
         except Exception as e:
